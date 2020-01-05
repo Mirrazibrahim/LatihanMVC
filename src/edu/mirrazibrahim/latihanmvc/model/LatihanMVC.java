4e475b24214e98ff7f117b623c846d2961c0f526
Mirraz Ibrahim <55575435+Mirrazibrahim@users.noreply.github.com>
2020-01-05 10:59:56 +0700

    Add files via upload

---
+++ src/edu/mirrazibrahim/latihanmvc/model/LatihanMVC.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mirrazibrahim.latihanmvc.model;
+
+import edu.mirrazibrahim.latihanmvc.view.PelangganView;
+
+/**
+ *
+ * @author
+ * NIM : 10118056
+ * NAMA : MIRRAZ IBRAHIM
+ * KELAS : IF-2
+ */
+public class LatihanMVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PelangganView pelanggan = new PelangganView();
+        pelanggan.setVisible(true);
+        
+    }
+    
+}
